3bcffe87b35e12d5f44e2d18949e680bb5926c4f
Mamoona Arshad <mamoona_arshad@hotmail.com>
2024-09-27 20:33:43 +0100

    adding the routing for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import Navbar from './components/Navbar';
+
 
 function App() {
-
-
   return (
-    <>
-     <Button>
-      Hello
-     </Button>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+
+    </Box>
   );
 }
 
+//NavBar is above the routes so I can see it on all routes
 export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"centre"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product store</Link>
+        </Text>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
