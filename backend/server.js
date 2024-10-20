fcda9c21a11cee4778d3f2363058a1de609203e1
Mamoona Arshad <mamoona_arshad@hotmail.com>
2024-10-20 13:56:14 +0100

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 import { connectDB } from './config/db.js'
 
 import router from "../routes/product.routes.js";
@@ -8,9 +9,18 @@ import router from "../routes/product.routes.js";
 
 dotenv.config();
 const app = express();
+const _dirname =path.resolve()
 app.use(express.json()); //allows json data to be parsed in the body
 const PORT = process.env.port || 5001
 app.use("/api/products", router)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+ res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    })
+}
+
 
 app.listen(PORT, () => {
     connectDB();
